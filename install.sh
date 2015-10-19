10c56942e13d41c41612cd66690e4420ee84b8bc
Edgar Vedvk <edgar.vedvik@gmail.com>
2015-10-19 15:03:01 +0200

    added .fehbg

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ rm ~/.profile
 rm ~/.Xresources
 rm -r ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.fehbg ~/.fehbg
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/.Xresources ~/.Xresources
 ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.fehbg/ ~/.fehbg
 
 git submodule init
 git submodule update
